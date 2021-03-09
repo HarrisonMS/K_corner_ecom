9f59462c2024437e02b5c239603cc3775dcd5a78
Harrison <hmseaborn@gmail.com>
2021-03-09 13:00:25 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,24 @@ module.exports = {
 			directory: "./backend/data/seeds",
 		},
 	},
+	production: {
+		client: "sqlite3",
+		useNullAsDefault: true,
+		connection: {
+			filename: "./data/kcorner2.db3",
+		},
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run("PRAGMA foreign_keys = ON", done);
+			},
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
+	},
 };
 
 // module.exports = {
