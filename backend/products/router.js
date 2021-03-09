2889cd9b926d13fd701a0861cf46385afb69f88b
Harrison <hmseaborn@gmail.com>
2021-03-09 13:30:55 -0800

    Update router.js

--- backend/products/router.js
+++ backend/products/router.js
@@ -16,7 +16,7 @@ router.get("/:id", (req, res) => {
 	Products.getById(id)
 		.then((product) => {
 			console.log(product);
-			res.status(200).json({ proudct });
+			res.status(200).json({ product });
 		})
 		.catch((err) => {
 			res.status(400).json({ err: err.message });
